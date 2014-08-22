29f7b3ec654f9e85829838702d917bed21701800
Zachary Kessin <zkessin@gmail.com>
2014-08-22 13:07:58 +0300

    switch to generated tests

--- test/lang_tests.erl
+++ test/lang_tests.erl
@@ -4,36 +4,25 @@
 -compile(export_all).
 
 
-swipl_comparison_test() ->
+swipl_comparison_test_() ->
     PLFiles     = get_files(),
     [begin
 	 Cmd         = iolist_to_binary(
 			 io_lib:format("swipl -q -f ../test/lang_tests/~s -g \"test('~s'),!;halt(1)\" -t halt",[File,File])),
 
-	 ?assertCmd(binary_to_list(Cmd)),
-	 true
-     end || File <-PLFiles],
-    true.
+	 ?_assertCmd(binary_to_list(Cmd))
+     end || File <-PLFiles].
+
+
     
 	 
-prop_lang_test() ->
+prop_lang_test_() ->
     PLFiles     = get_files(),
     {ok,PL} = erlog:new(),
     [begin
 	 {ok,PL1} = erlog:consult(PL,"../test/lang_tests/"++ File),
-	 case  erlog:prove(PL1, {test,File}) of
-	     {{succeed, _},_} ->
-		 true;
-	     {fail, _} ->
-		 ?debugFmt("File ~p test/1 fails ~n",[File]),
-		 ?assert(false)
-	 end
-     end || File <- PLFiles],
-    true.
-
-
-
-
+	 ?_assertMatch({{succeed, _},_},  erlog:prove(PL1, {test,File}))
+     end || File <- PLFiles].
 
 
 
